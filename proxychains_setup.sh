ec6d2b70eeecf90de007bd6ed8ac6bb332e1add8
Eric Valdez Jr <128732768+pyx9999@users.noreply.github.com>
2023-03-26 14:57:48 -0700

    Update proxychains_setup.sh

--- proxychains_setup.sh
+++ proxychains_setup.sh
@@ -9,6 +9,3 @@ apt upgrade -y
 echo | apt install tor proxychains
 updatedb
 echo "socks5  127.0.0.1 9050" | cat >> /etc/proxychains.conf
-updatedb
-sed '18 s/./#&/' /etc/proxychains.conf > /etc/proxychains.conf
-sed '25 s/#//' /etc/proxychains.conf > /etc/proxychains.conf
